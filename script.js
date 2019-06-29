a2e6b0dcfbcbf1eae2bdd22feeab572963167efd
barthook <50553996+barthook@users.noreply.github.com>
2019-06-29 16:18:59 +0200

    Final version - 1

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-	var poz1 = window.pageYOffset;
+// Hide navbar on scroll
 	
-	window.onscroll = function(){
-	var poz2 = window.pageYOffset;
+	let poz1 = window.pageYOffset;
+	
+function navScroll(){
+	let poz2 = window.pageYOffset;
 	if ( poz1 > poz2 ){
-		document.getElementById("naw").style.top = "0";
+		document.getElementById("myTopnav").style.top = "0";
 	} else {
-		document.getElementById("naw").style.top = "-52px";
+		document.getElementById("myTopnav").style.top = "-54px";
 	}
 		poz1 = poz2;
 }
+
+
+// Show responsive topnav onclick
+
+function myFunction() {
+	let x = document.getElementById("myTopnav");
+	if (x.className === "topnav"){
+		x.className += " responsive";
+		}else{
+		x.className = "topnav";
+	}
+}
+
+// show scroll button
+
+function btnScroll() {
+	if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+		document.getElementById("scrollTop").style.display = "block";
+	} else {
+		document.getElementById("scrollTop").style.display = "none";
+	}
+}
+
+// when clicked, scrolls document to top and hides an alement
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+
+// active functions on scroll
+
+window.onscroll = function() {
+	navScroll()
+	btnScroll()
+	};
